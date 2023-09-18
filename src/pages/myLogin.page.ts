497caeecc00cfdd4a482dcc0e52cfb8e1c49beae
Colin Sullivan <cms.colin.sullivan@gmail.com>
2023-09-18 13:50:15 -0500

    init pages, with example

---
+++ src/pages/myLogin.page.ts
@@ -0,0 +1,17 @@
+
+
+// locators
+
+// elementId of page 00000000-0000-0022-0000-000300000004
+
+// 0 textbox - username
+// xpath = /hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.widget.ScrollView/android.widget.EditText[1]/android.view.View/android.widget.TextView
+
+// 1 textbox - password
+// xpath = /hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.widget.ScrollView/android.widget.EditText[2]
+
+// 2 textbox - age
+// xpath = /hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.widget.ScrollView/android.widget.EditText[3]
+
+// 3 button - "sign in"
+// xpath = /hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.widget.ScrollView/android.view.View
